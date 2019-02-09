fb88562c6cc3e1dec794b289b0b61b3c53692410
khalid <shirwadheere12@hotmial.com>
2019-02-09 13:19:22 +0300

    [ft-post-meetup#163084619] refactoring

--- app/tests/base_test.py
+++ app/tests/base_test.py
@@ -1,8 +1,6 @@
 from unittest import TestCase
 from app import create_app
-from app.api.v1.models.meetups import MEETUPS_LIST 
-from app.api.v1.models.questions import QUESTIONS_LIST
-from app.api.v1.models.users import USERS_LIST
+from app.api.v1.models.meetups import MEETUPS_LIST
 class BaseTestCase(TestCase):
 
     def setUp(self):
@@ -13,5 +11,4 @@ class BaseTestCase(TestCase):
 
     def tearDown(self):
         del MEETUPS_LIST[:]
-        del QUESTIONS_LIST[:]
-        del USERS_LIST[:]
+        
