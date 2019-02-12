e1ce0c071072b9aae9a4516d6810ed81cd5f72ba
khalid <shirwadheere12@hotmial.com>
2019-02-12 16:04:02 +0300

    r

--- app/tests/test_meetup.py
+++ app/tests/test_meetup.py
@@ -21,4 +21,4 @@ class TestMeetups(BaseTestCase):
             response3 = self.client.post('/api/v1/meetups', data=json.dumps(dict()), content_type='application/json')
             response_data =  json.loads(response3.data)
             self.assertEqual("Fields cannot be empty", response_data["message"])
-            self.assertEqual(response3.status_code, 400)
+            self.assertEqual(response3.status_code, 400) 
