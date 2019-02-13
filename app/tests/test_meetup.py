4cdbb51125bd2f0471fd7bce382f6e9c7125549e
khalid <shirwadheere12@hotmial.com>
2019-02-13 18:44:59 +0300

    R

--- app/tests/test_meetup.py
+++ app/tests/test_meetup.py
@@ -3,16 +3,7 @@ import datetime
 from base_test import BaseTestCase
 
 class TestMeetups(BaseTestCase):
-
-
-
-
-
-
-            response3 = self.client.post('/api/v1/meetups', data=json.dumps(dict()), content_type='application/json')
-            response_data =  json.loads(response3.data)
-            self.assertEqual("Fields cannot be empty", response_data["message"])
-            self.assertEqual(response3.status_code, 400) 
-
-    
-
+    def test_upcoming_meetings(self):
+            self.client.post('/api/v1/meetups', data=json.dumps(dict(meetup_id=100, location='mombasa',images='[image1, image2, image3]', topic='tech', tags='[data, sms, swahili pot]', happening_on='10/12/2018', created_on=str(datetime.datetime.now()) )), content_type='application/json')
+            result = self.client.get('/api/v1/meetups/upcoming')
+            self.assertEqual(result.status_code, 200)
